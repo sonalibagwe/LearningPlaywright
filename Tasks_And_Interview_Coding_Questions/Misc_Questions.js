479dfe0c2aa556b2a3bc1feada2c067ad8fd6608
Sonali <sonaliabagwe@gmail.com>
2026-03-03 08:25:56 +0530

    Add misc coding questions

---
+++ Tasks_And_Interview_Coding_Questions/Misc_Questions.js
@@ -0,0 +1,37 @@
+console.log(typeof NaN);
+// console.log(x);
+// let x=5;
+
+// for(i=1; i<=5; i++) {
+//     var a = 10;
+//     console.log("in", a);
+// }
+// console.log("out",a);
+
+
+// let y;
+// console.log(y);
+
+// const arr=[1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+
+// console.log('5' +3);
+// console.log('5' -3);
+// console.log(true + true);
+
+// console.log(0 || 'hello');
+// console.log(0 ?? 'hello');
+
+
+// if ('') { console.log('yes'); } else { console.log('no'); }
+
+// console.log(Boolean([]));
+
+
+let status = 200; 
+if (status === 200) { 
+    console.log("OK"); 
+} else if (status === 404) { 
+    console.log("Not Found"); 
+} else { console.log("Other"); } 
